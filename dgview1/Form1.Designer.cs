1732b50e9858fec7169daf0c22010af10439e280
Murathan Arat <murathanarat.is@hotmail.com>
2025-04-13 20:51:26 +0300

    Add project files.

---
+++ dgview1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.TB2 = new System.Windows.Forms.TextBox();
+            this.TB3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.DGV1 = new System.Windows.Forms.DataGridView();
+            this.TB4 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TB1
+            // 
+            this.TB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TB1.Location = new System.Drawing.Point(12, 12);
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(40, 26);
+            this.TB1.TabIndex = 0;
+            // 
+            // TB2
+            // 
+            this.TB2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TB2.Location = new System.Drawing.Point(58, 12);
+            this.TB2.Name = "TB2";
+            this.TB2.Size = new System.Drawing.Size(79, 26);
+            this.TB2.TabIndex = 2;
+            // 
+            // TB3
+            // 
+            this.TB3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TB3.Location = new System.Drawing.Point(143, 12);
+            this.TB3.Name = "TB3";
+            this.TB3.Size = new System.Drawing.Size(79, 26);
+            this.TB3.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(73, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "değiştir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(332, 330);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 23);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "KAPAT";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(283, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "label1";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(12, 330);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(73, 23);
+            this.button9.TabIndex = 21;
+            this.button9.Text = "temizle";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // DGV1
+            // 
+            this.DGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV1.Location = new System.Drawing.Point(12, 73);
+            this.DGV1.Name = "DGV1";
+            this.DGV1.Size = new System.Drawing.Size(393, 251);
+            this.DGV1.TabIndex = 22;
+            this.DGV1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // TB4
+            // 
+            this.TB4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TB4.Location = new System.Drawing.Point(228, 12);
+            this.TB4.Name = "TB4";
+            this.TB4.Size = new System.Drawing.Size(40, 26);
+            this.TB4.TabIndex = 30;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(428, 368);
+            this.Controls.Add(this.TB4);
+            this.Controls.Add(this.DGV1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TB3);
+            this.Controls.Add(this.TB2);
+            this.Controls.Add(this.TB1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DGV1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.TextBox TB2;
+        private System.Windows.Forms.TextBox TB3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.DataGridView DGV1;
+        private System.Windows.Forms.TextBox TB4;
+    }
+}
+
---
+++ dgview1/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Collections;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection.Emit;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public static ArrayList no = new ArrayList() { 11, 12, 13, 14, 17, 18, 19, 20 };
+        public static ArrayList ad = new ArrayList() { "ali", "osman", "mehmet", "yunus", "fatma", "nurcan", "eslem", "sude" };
+        public static ArrayList soyad = new ArrayList() { "AK", "KAYA", "MOR", "TOR", "KAR", "AS", "KOR", "KEL" };
+        public static ArrayList ort = new ArrayList() { 85, 50, 58, 55, 68, 65, 46, 48 };
+
+        int i, j, s;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvolustur();
+            yukle();
+            lbyaz();
+
+        }
+        private void dgvolustur()
+        {
+            DGV1.ColumnCount = 4;
+            DGV1.Columns[0].Name = "No";
+            DGV1.Columns[1].Name = "Ad";
+            DGV1.Columns[2].Name = "Soyad";
+            DGV1.Columns[3].Name = "Ort";
+
+            DGV1.Columns[0].Width = 50;
+            DGV1.Columns[1].Width = 125;
+            DGV1.Columns[2].Width = 125;
+            DGV1.Columns[3].Width = 80;
+
+            DGV1.ReadOnly = true;
+            DGV1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            DGV1.MultiSelect = false;
+        }
+        private void yukle()
+        {
+            for (s = 0; s < ad.Count; s++)
+            {
+                DGV1.Rows.Add(no[s], ad[s], soyad[s], ort[s]);
+            }
+
+            i = ad.Count - 1;
+            j = i;
+            lbyaz();
+        }
+        private void button1_Click(object sender, EventArgs e)//ekle
+        {
+            if (hataA() == 1)
+            {
+                i++;
+                j = i;
+                ekle();
+                lbyaz();
+                TB1.Focus();
+            }
+        }
+        private void ekle()
+        {
+            DGV1.Rows.Add(TB1.Text, TB2.Text, TB3.Text, TB4.Text);
+        }
+        private void degistir()
+        {
+            DGV1.Rows[j].Cells[0].Value = TB1.Text;
+            DGV1.Rows[j].Cells[1].Value = TB2.Text;
+            DGV1.Rows[j].Cells[2].Value = TB3.Text;
+            DGV1.Rows[j].Cells[3].Value = TB4.Text;
+        }
+        private void button3_Click(object sender, EventArgs e)//degistir
+        {
+            if (hataA() == 1)
+            {
+                degistir();
+                lbyaz();
+                TB1.Focus();
+            }
+        }
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            j = DGV1.CurrentRow.Index;
+            lbyaz();
+            if (DGV1.CurrentRow != null && j <= i)
+            {                        
+                TB1.Text = DGV1.Rows[j].Cells[0].Value.ToString();
+                TB2.Text = DGV1.Rows[j].Cells[1].Value.ToString();
+                TB3.Text = DGV1.Rows[j].Cells[2].Value.ToString();
+                TB4.Text = DGV1.Rows[j].Cells[3].Value.ToString();             
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)//sil
+        {
+            if (DGV1.SelectedRows.Count > 0 && j >= 0 && j <= i)
+            {              
+                DGV1.Rows.RemoveAt(j);
+                i--;
+                temizle();
+                lbyaz();
+            }
+            else
+            {
+                MessageBox.Show("Eleman seçiniz.");
+            }
+            TB1.Focus();
+        }
+        private void lbyaz()
+        {
+            DGV1.CurrentCell = DGV1.Rows[j].Cells[0];
+            label1.Text = (j + 1) + " / " + (i + 1);
+        }
+        private void temizle()
+        {
+
+            TB1.Text = ""; TB2.Text = "";
+            TB3.Text = ""; TB4.Text = "";      
+        }
+        private int hataA()
+        {
+            if (TB1.Text == "" || TB2.Text == "" || TB3.Text == "" || TB4.Text == "")
+            {
+                MessageBox.Show("Hata: Alanları boş bırakmayınız!");
+                return 0;
+            }
+            return 1;
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            temizle();
+        }
+    }
+}
